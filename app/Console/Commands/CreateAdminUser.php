86448cc1e2ed33a3f35de1076776aa1c9bf42cc9
Sasa Fister <sashafishter@gmail.com>
2021-02-11 10:46:00 +0100

    Add command for creating admin user

---
+++ app/Console/Commands/CreateAdminUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateAdminUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create admin user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::firstOrCreate(['name' => 'Admin'],
+        [
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin123'),
+        ]);
+
+        $this->info('Admin user has created successfully!');
+    }
+}
